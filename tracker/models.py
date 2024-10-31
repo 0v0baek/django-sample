f085801ac1a70eefa059230a21e52e79384f1267
0v0baek <qorqor4592@gmail.com>
2024-10-31 17:51:45 +0900

    :sparkles:Feat:Add tracker model
    ー新しいモデル生成(BodyData)
    ー.gitignoreアップデート(migration、DBファイル)

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class BodyData(models.Model):
+    class Meta:
+        db_table = 'my_body_data'
+
+    name = models.CharField(max_length=72, default='ユーザー', verbose_name="名前")
+    height = models.PositiveIntegerField(null=True, verbose_name='身長')
+    weight = models.PositiveIntegerField(null=True, verbose_name='体重')
+    healthy_weight = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='適性体重')
+    bmi = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='BMI')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='計測日')
